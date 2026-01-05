5db0a8a44b414dbfba571b8cb7948bba54fd7d45
Kritika Karma <your_email@example.com>
2026-01-05 21:32:00 +0530

    Use process.env.PORT for Render

--- app.js
+++ app.js
@@ -153,7 +153,12 @@ app.use((err,req,res,next)=>{
 
   res.status(statusCode).render("error.ejs" ,{message});
 });
-app.listen(8080, () => {
-    console.log("server is listening on port 8080");
-});
+// app.listen(8080, () => {
+//     console.log("server is listening on port 8080");
+// });
 
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
